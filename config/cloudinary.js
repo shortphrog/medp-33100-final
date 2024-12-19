31fca6673333ef9273f2be5a18fd16a009033e27
shortphrog <81057915+shortphrog@users.noreply.github.com>
2024-12-18 21:16:30 -0500

    initial project

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -10,36 +10,7 @@ async function connectToCloudnary() {
         api_secret: 'GzkeMZ0ynDanYZKRjBo-uHDeD2M' 
     });
     
-    // // Upload an image
-    //  const uploadResult = await cloudinary.uploader
-    //    .upload(
-    //        'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
-    //            public_id: 'shoes',
-    //        }
-    //    )
-    //    .catch((error) => {
-    //        console.log(error);
-    //    });
-    
-    // console.log(uploadResult);
-    
-    // // Optimize delivery by resizing and applying auto-format and auto-quality
-    // const optimizeUrl = cloudinary.url('shoes', {
-    //     fetch_format: 'auto',
-    //     quality: 'auto'
-    // });
-    
-    // console.log(optimizeUrl);
-    
-    // // Transform the image: auto-crop to square aspect_ratio
-    // const autoCropUrl = cloudinary.url('shoes', {
-    //     crop: 'auto',
-    //     gravity: 'auto',
-    //     width: 500,
-    //     height: 500,
-    // });
-    
-    // console.log(autoCropUrl); 
+
     return cloudinary; 
 }
 
